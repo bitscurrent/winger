b108d993168a218c1393ae014e7b40b3e041706c
dilip <dilipboidya.office@gmail.com>
2024-08-04 21:48:45 +0530

    setup db

--- server/database/db.js
+++ server/database/db.js
@@ -1,39 +1,21 @@
 import mongoose from 'mongoose';
 
 const connectDB = async () => {
+  const mongoURI = process.env.MONGO_URI
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(mongoURI);
+    console.log("Database Connected");
 
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
+    // Listen for connection events and handle errors
+    mongoose.connection.on('connected', () => {
+      console.log('Mongoose connected to DB');
+    });
+
   } catch (error) {
     console.error(`Error: ${error.message}`);
     process.exit(1); // Exit process with failure
   }
 };
 
-// Listen for connection events and handle errors
-mongoose.connection.on('connected', () => {
-  console.log('Mongoose connected to DB');
-});
-
-mongoose.connection.on('error', (err) => {
-  console.error(`Mongoose connection error: ${err}`);
-});
-
-mongoose.connection.on('disconnected', () => {
-  console.log('Mongoose disconnected from DB');
-});
-
-// Gracefully close the connection when the Node.js process ends
-process.on('SIGINT', async () => {
-  try {
-    await mongoose.connection.close();
-    console.log('Mongoose connection closed on app termination');
-    process.exit(0);
-  } catch (error) {
-    console.error(`Error closing Mongoose connection: ${error.message}`);
-    process.exit(1);
-  }
-});
-
 export default connectDB;
+
--- server/index.js
+++ server/index.js
@@ -2,13 +2,13 @@
 import express from "express"
 import { configDotenv } from "dotenv"
 import connectDB from "./database/db.js"
+import mongoose from "mongoose"
 
 configDotenv()
 const port = process.env.PORT
 const app=express()
 
 connectDB()
-
 
 
 app.listen(port,()=>console.log(`Server is running at port ${port}`))
