6f6bee93781d8a622ba46517f02b6080f96f6125
bitscurrent <dilipboiya.office@gmail.com>
2024-09-08 00:17:50 +0530

    add hostname in config file

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"t7dsqGCq230q8kf5seKNTLS5YcdF897MhNT8FnFuUa4=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"5baSHkVriHbGjvvABWh5R0QO6UFrzn/q/EUzqjQmCLI=\"}"
--- src/pages/hostname.js
+++
@@ -1,3 +0,0 @@
-// export const URL="http:localhost:3000"
-
-export const URL="https://ratan.vercel.app"
